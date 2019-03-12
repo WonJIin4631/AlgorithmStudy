29bf46aa008422ae12e5a8691e45fbfb800be587
WonJin4631 <smartwj@naver.com>
2019-03-12 23:23:25 +0900

    0312

---
+++ dodo/dodo/bj1700.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int N, K;
+int multap[100 + 1];
+int uselist[100 + 1];
+int ans=0;
+
+int main() {
+	cin >> N >> K;
+	for (int i = 0; i < K; i++) {
+		cin >> uselist[i];
+	}
+	for (int i = 0; i < K; i++) {
+		bool isUsed = 0;
+		for (int j = 0; j < N; j++) {
+			if (multap[j] == uselist[i])
+			{
+				isUsed = 1;
+				break;
+			}
+		}
+		if (isUsed)
+			continue;
+		for (int j = 0; j < N; j++) {
+			if (!multap[j]) {
+				multap[j] = uselist[i];
+				isUsed = 1;
+				break;
+			}
+		}
+		if (isUsed)
+			continue;
+		int idx, deviceidx = -1;
+		for (int j = 0; j < N; j++) {
+			int lastidx = 0;
+			for (int k = i + 1; k < K; k++) {
+				if (multap[j] == uselist[k])
+					break;
+				lastidx++;
+			}
+			if (lastidx > deviceidx) {
+				idx = j;
+				deviceidx = lastidx;
+			}
+
+		}
+		ans++;
+		multap[idx] = uselist[i];
+	}
+	cout << ans << endl;
+}
