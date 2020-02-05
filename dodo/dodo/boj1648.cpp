ca33b0b8d02659617a50a4cdd0e5479dc445a9eb
PARK WONJIN <pwj4119@gmail.com>
2020-02-05 16:29:18 +0900

    0205

--- dodo/dodo/boj1648.cpp
+++ dodo/dodo/boj1648.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
+/*
+DP문제같음
 
+점화식을 찾기
+
+*/
+long long DP[15][15]{ 0 };
+int N, M;
 int main() {
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
 
+		}
+	}
 	return 0;
 }
--- dodo/dodo/prg42578.cpp
+++ dodo/dodo/prg42578.cpp
@@ -1,32 +1,32 @@
 #include <string>
 #include <vector>
 #include<map>
+#include<algorithm>
 #include<iostream>
 using namespace std;
 /*
-현재 가지고 있는 옷의 수
+Map을 이용하여 의상의 종류에 맞는 Idx 번호를 부여
+종류별 (의상의 개수+1) 곱한뒤 마지막에 1을 빼면 조합의 수가 나옴
 */
-
-
-void solve(int cnt,int idx) {
-
-}
 int solution(vector<vector<string>> clothes) {
-	int answer = 0;
-	vector<int> list(30, 0);
+	int answer = 1;
+	vector<vector<string>> list;
+	list.resize(30);
 	map<string, int> Map;
 	int idx = 0;
 	for (int i = 0; i < clothes.size(); i++) {
 		if (Map.find(clothes[i][1]) == Map.end()) {
-			list[idx]++;
+			list[idx].push_back(clothes[i][0]);
 			Map.insert(make_pair(clothes[i][1], idx++));
 		}
 		else {
 			int pos = Map.find(clothes[i][1])->second;
-			list[pos]++;
+			list[pos].push_back(clothes[i][0]);
 		}
 	}
-	list.resize(idx);
-	cout << list.size();
+	for (int i = 0; i < idx; i++) {
+		answer *= (list[i].size() + 1);
+	}
+	answer -= 1;
 	return answer;
 }
