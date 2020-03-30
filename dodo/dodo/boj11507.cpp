6d7bd6eacf226121b4077163d6fbeb29de6f3748
PARK WONJIN <smartwj@naver.com>
2020-03-30 16:55:56 +0900

    0330

---
+++ dodo/dodo/boj11507.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int N;
+int ans = 0;
+int main() {
+	int dp[1001][10] = { 0 };
+	cin >> N;
+
+	for (int i = 0; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = j; k < 10; k++) {
+				dp[i][j] = (dp[i][j] + dp[i - 1][k]) % 10007;
+			}
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		ans = (ans + dp[N][i]) % 10007;
+	}
+	cout << ans << '\n';
+	return 0;
+}
--- dodo/dodo/boj2146.cpp
+++ dodo/dodo/boj2146.cpp
@@ -1,12 +1,39 @@
 #include<iostream>
+#include<queue>
+#include<vector>
 using namespace std;
 
+/*
+섬을 구분하며 섬의 외각부분의 좌표를 저장한다.
+섬의 외각지역의 모든부분과 다른 섬의 외각지역의 거리를 계산
+
+
+*/
+
 int map[101][101];
-int divmap[101][101];
+int visit[101][101];
 int N;
+int dx[] = { 0,0,-1,1 };
+int dy[] = { -1,1,0,0 };
+int island_cnt = 0;
+typedef pair<int, int> pii;
+vector<pii> island[10001];
+int ans = 987654321;
+void solve(int from, int to) {
+	if (to != -1) {
+		for (int i = 0; i < island[from].size(); i++) {
+			for (int j = 0; j < island[to].size(); j++) {
+				int dist = abs(island[from][i].first - island[to][j].first) + abs(island[from][i].second - island[to][j].second) - 1;
+				ans = min(ans, dist);
 
-
-
+			}
+		}
+		return;
+	}
+	for (int i = from + 1; i < island_cnt; i++) {
+		solve(from, i);
+	}
+}
 int main() {
 	cin >> N;
 	for (int i = 0; i < N; i++) {
@@ -14,4 +41,44 @@ int main() {
 			cin >> map[i][j];
 		}
 	}
+	queue<pii> q;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (map[i][j] == 1) {
+				if (visit[i][j] == 0) {
+					visit[i][j] = 1;
+					q.push(pii(i, j));
+					while (!q.empty()) {
+						int y = q.front().first;
+						int x = q.front().second;
+						q.pop();
+						bool flag = false;
+						for (int k = 0; k < 4; k++) {
+							int ny = y + dy[k];
+							int nx = x + dx[k];
+							if (ny >= 0 && ny < N&&nx >= 0 && nx < N) {
+								if (map[ny][nx] == 1) {
+									if (visit[ny][nx] == 0) {
+										visit[ny][nx] = 1;
+										q.push(pii(ny, nx));
+									}
+								}
+								else {
+									flag = true;
+								}
+							}
+						}
+						if (flag) {
+							island[island_cnt].push_back(pii(y, x));
+						}
+					}
+					island_cnt++;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < island_cnt; i++) {
+		solve(i, -1);
+	}
+	cout << ans << '\n';
 }
