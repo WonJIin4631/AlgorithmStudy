c8e330d26f607f782c0df9d2d72821ae0188fc73
PARK WONJIN <smartwj@naver.com>
2020-09-28 00:26:44 +0900

    0928

--- dodo/dodo/boj11056.cpp
+++ dodo/dodo/boj11056.cpp
@@ -2,29 +2,39 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-/*
-2개의 문자열의 최대 부분 문자열의 길이들을 다 구한뒤
-2개의 문자열 길이에서 뺀다.
-*/
-int main() {
-	string s1;
-	string s2;
-	cin >> s1 >> s2;
-	int dp[1002][1002] = { 0 };
-	int ans = 0;
-	for (int i = 1; i <= s1.length(); i++) {
-		for (int j = 1; j <= s2.length(); j++) {
-			if (s1[i - 1] == s2[j - 1]) {
-				dp[i][j] = dp[i - 1][j - 1] + 1;
-				ans =max(ans,dp[i][j]);
-			}
-		}
+
+string s1;
+string s2;
+int dp[1002][1002] = { 0 };
+int lcs(int i, int j) {
+	if (i == -1 || j == -1) {
+		return 0;
 	}
-	for (int i = 1; i <= s1.length(); i++) {
-		for (int j = 1; j <= s2.length(); j++) {
+	if (dp[i][j] != -1) {
+		return dp[i][j];
+	}
+	else if (s1[i] == s2[j]) {
+		return dp[i][j] = lcs(i - 1, j - 1) + 1;
+	}
+	else if (s1[i] != s2[j]) {
+		return dp[i][j] = max(lcs(i - 1, j), lcs(i, j - 1));
+	}
+}
+int main() {
+	cin >> s1 >> s2;
+	for (int i = 0; i < 1002; i++)
+		for (int j = 0; j < 1002; j++)
+			dp[i][j] = -1;
+	int ans = s1.length() + s2.length() - lcs(s1.length() - 1, s2.length() - 1);
+	/*
+	int maxLen = max(s1.length(), s2.length());
+	for (int i = 0; i < maxLen; i++) {
+		for (int j = 0; j < maxLen; j++) {
 			cout << dp[i][j] << ' ';
 		}
 		cout << '\n';
 	}
+	*/
 	cout << ans << '\n';
+	return 0;
 }
